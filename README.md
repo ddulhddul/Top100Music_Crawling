c94e51cb0ffca360c55959080b160cc9ca4402d0
ddulh <rjw88@naver.com>
2017-05-01 18:10:31 +0900

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Command
+```linux
+npm init
+npm install express --save
+npm install jade --save
+npm install request cheerio --save
+npm install urlencode --save
+```
+
+## get melon chart info
+[https://developers.skplanetx.com/apidoc/kor/melon/chart/](https://developers.skplanetx.com/apidoc/kor/melon/chart/)
+
+## when youtube ended
+[how to control ?](http://stackoverflow.com/questions/24964232/how-to-fire-function-on-embedded-youtube-iframes-end)
---
+++ app.js
@@ -0,0 +1,82 @@
+let express = require('express')
+let urlencode = require('urlencode');
+let request = require('request');
+let cheerio = require('cheerio');
+
+let app = express()
+app.use(express.static(__dirname +'/css'));
+app.set('view engine', 'jade');
+app.set('views', 'html')
+
+let songList = [];
+
+app.get('/song', (req, res)=>{
+	urlRequest('http://www.melon.com/chart/')
+    .then(($)=>{
+        if(!$) res.send('Error')
+        let postElements = $('form table .wrap_song_info');
+        let result = [];
+        let index = 0;
+        postElements.each(function(i, obj) {
+            let $obj = $(obj);
+            let song = $obj.find('.rank01 a').text(), singer = $obj.find('.rank02 a span').first().text();
+            let encodedSrchparam = urlencode(song+' '+singer);
+            result.push({
+                num: index++,
+                song : song,
+                singer : singer,
+                url : 'https://www.youtube.com/results?search_query='+encodedSrchparam,
+                temp: res
+            })
+        });
+        songList = result;
+        res.render('index', {
+            result: songList
+        });
+    }, (error)=>{
+        res.send('url request Call Erro :::\n'+error);
+    });
+})
+
+app.get('/song/:index', (req,res)=>{
+    let index = req.params.index;
+    if(!songList[index]) res.redirect('/song/')
+    for (let i = 0, len = songList.length; i < len; i++) {
+        if(i == index) songList[i].class = 'success'
+        else songList[i].class = ''
+    }
+    console.log('comecomecomecome ::', songList[index], index)
+    urlRequest(songList[index].url)
+    .then(($)=>{
+        if(!$) res.send('Error')
+        let href = $('#results ol li ol li a').first().attr('href');
+        href = href.replace('/watch?v=','');
+        res.render('index', {
+            result: songList, 
+            url: "https://www.youtube.com/embed/"+href+"?autoplay=1"
+        });
+    }, (error)=>{
+        res.send('url request Call Erro :::\n'+error);
+    });
+})
+
+let urlRequest = function (param) {
+	return new Promise(function (resolve, reject) {
+		let url = param;
+        request(url, function (error, response, body) {
+            try {
+                if (error) reject('Unexpected Error :::');
+                let $ = cheerio.load(body);
+                resolve($);
+                
+            } catch(e) {
+                resolve();
+                console.log('request Error :::', e);
+            }
+        })
+	});
+};
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
