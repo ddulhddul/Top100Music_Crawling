0d691eb9f96209032173bcbe28a8602bd635d1d8
ddulh <rjw88@naver.com>
2017-05-08 15:52:56 +0900

    apply mongoose (url request per 1hour)

---
+++ Chart.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var chartSchema = new Schema({
+    yymmddhh: String,
+    num: Number,
+    song: String,
+    singer: String,
+    url: String,
+    videoId: String
+});
+
+module.exports = mongoose.model('chart', chartSchema);
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let urlencode = require('urlencode');
 let request = require('request');
 let cheerio = require('cheerio');
 let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let Chart = require('./Chart');
 
 let app = express()
 app.use(express.static(__dirname +'/css'));
@@ -10,49 +12,126 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'jade');
 app.set('views', 'html');
 
+let db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+mongoose.connect('mongodb://127.0.0.1/chartdb');
+
+function lpadNum(num){
+    if(num < 10) return '0'+num
+    else return String(num)
+}
+
+function getYymmddhh(param){
+    let date = param || new Date();
+    return date.getFullYear()+
+        lpadNum(date.getMonth()+1)+
+        lpadNum(date.getDate())+
+        lpadNum(date.getHours())
+}
 
 app.get('/song', (req, res)=>{
-	urlRequest('http://www.melon.com/chart/')
-    .then(($)=>{
-        if(!$) res.send('Error')
-        let postElements = $('form table .wrap_song_info');
-        let result = [];
-        let index = 0;
-        postElements.each(function(i, obj) {
-            let $obj = $(obj);
-            let song = $obj.find('.rank01 a').text(), singer = $obj.find('.rank02 a span').first().text();
-            let encodedSrchparam = urlencode(song+' '+singer);
-            result.push({
-                num: ++index,
-                song : song,
-                singer : singer,
-                url : 'https://www.youtube.com/results?search_query='+encodedSrchparam
-            })
-        });
-        res.render('index', {
-            result: result,
-            index: 0,
-            totNum: result.length
-        });
-        // res.redirect('/song/0')
-    }, (error)=>{
-        res.send('url request Call Erro :::\n'+error);
-    });
+	let yymmddhh= getYymmddhh();
+    Chart.find({yymmddhh: yymmddhh},(err,result)=>{
+        if(err) console.log('chart find error...', err)
+
+        if(err || !result || result.length === 0){
+            console.log(`${yymmddhh} result not exists`)
+            getChartByUrlRequest(res);
+        }else{
+            
+            console.log(`${yymmddhh} result exists`)
+            res.render('index', {
+                result: result,
+                index: 0,
+                totNum: result.length
+            });
+        }
+
+    })
 })
 
-app.post('/song/change', (req,res)=>{
-    urlRequest(req.body.url)
-    .then(($)=>{
-        if(!$) res.send({err:'Error'})
-        // let href = $('#results ol li ol li a').first().attr('href');
-        let href = $('.yt-lockup-video a').first().attr('href');
-        href = href.replace('/watch?v=','');
-        res.send({
-            url: href
+function getChartByUrlRequest(res){
+    Chart.remove({},(err)=>{
+        if(err) console.log('chart remove error...',err)
+
+        console.log('date : ', new Date(), 'chart url request.. ')
+        urlRequest('http://www.melon.com/chart/')
+        .then(($)=>{
+            if(!$) res.send('Error')
+            let postElements = $('form table .wrap_song_info');
+            let result = [];
+            let index = 0;
+            let yymmddhh = getYymmddhh()
+
+            postElements.each(function(i, obj) {
+                let $obj = $(obj);
+                let song = $obj.find('.rank01 a').text(), singer = $obj.find('.rank02 a span').first().text();
+                let encodedSrchparam = urlencode(song+' '+singer);
+                let param = {
+                    num: ++index,
+                    song : song,
+                    singer : singer,
+                    url : 'https://www.youtube.com/results?search_query='+encodedSrchparam
+                }
+                result.push(param)
+
+                let chart = new Chart();
+                chart.yymmddhh = yymmddhh;
+                chart.num = param.num;
+                chart.song = song;
+                chart.singer = singer;
+                chart.url = param.url;
+                chart.videoId = '';
+                chart.save((err,result)=>{
+                    if(err) console.log('chart insert error...'.err)
+                })
+            });
+            res.render('index', {
+                result: result,
+                index: 0,
+                totNum: result.length
+            });
+            // res.redirect('/song/0')
+        }, (error)=>{
+            res.send('url request Call Erro :::\n'+error);
         });
-    }, (error)=>{
-        res.send({err:'url request Call Erro :::\n'+error});
-    });
+    })
+}
+
+app.post('/song/change', (req,res)=>{
+    let body = req.body;
+    
+    Chart.findOne({yymmddhh:body.yymmddhh, num:body.num},(err,chart)=>{
+        if(!err && chart && chart.videoId){
+            console.log('date : ', new Date(), 'videoId exists', chart.videoId)
+            res.send({url: chart.videoId});
+        }else{
+            console.log('date : ', new Date(), 'videoId not exists')
+            urlRequest(body.url)
+            .then(($)=>{
+                if(!$) res.send({err:'Error'})
+                // let href = $('#results ol li ol li a').first().attr('href');
+                let href = $('.yt-lockup-video a').first().attr('href');
+                href = href.replace('/watch?v=','');
+
+                chart.videoId = href;
+                chart.save((err)=>{
+                    if(err) console.log('chart videoId update error...',err)
+                })
+
+                res.send({
+                    url: href
+                });
+            }, (error)=>{
+                res.send({err:'url request Call Erro :::\n'+error});
+            });
+        }
+    })
+
 })
 
 let urlRequest = function (param) {
