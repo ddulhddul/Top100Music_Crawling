48a5e800e6b877cb4bb5a6e6c3d339314da30cb0
Kitchu0401 <kitchu@naver.com>
2017-05-14 03:11:06 +0900

    updated batch script

--- update-formatted-date.js
+++ update-formatted-date.js
@@ -2,24 +2,25 @@ var mongoose = require('mongoose')
 var moment = require('moment')
 var Message = require('./model/Message')
 
-let db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-    // CONNECTED TO MONGODB SERVER
-    console.log("Connected to mongod server");
-});
+// let db = mongoose.connection;
+// db.on('error', console.error);
+// db.once('open', function(){
+//     // CONNECTED TO MONGODB SERVER
+//     console.log("Connected to mongod server");
+// });
 mongoose.connect('mongodb://127.0.0.1/chartdb');
 
-try {
-  Message
-  .find()
-  .snapshot()
-  .then((result) => {
-    result.forEach((message) => {
-      message.formattedDate = moment(message.date).locale('ko').format('LLL')
-      message.save()
+Message
+.find()
+.snapshot()
+.then((result) => {
+  result.forEach((message) => {
+    message.formattedDate = moment(message.date).locale('ko').format('LLL')
+    message
+    .save()
+    .then((saved) => {
+      console.log('[%s] formattedDate updated from %s >> %s', saved.seq, message.formattedDate, saved.formattedDate)
     })
   })
-} catch (error) {
-  console.error(error)
-}
+})
+.catch(console.error)
