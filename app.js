4c72d41672286765496a13e5de3bc59f5e40a8bf
ddulhddul <rjw88@naver.com>
2017-08-31 10:41:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ app.get('/song/change', (req,res)=>{
                 .then(($)=>{
                     if(!$) res.send({err:'Error'})
                     // let href = $('#results ol li ol li a').first().attr('href');
-                    let href = $('.yt-lockup-video a').first().attr('href');
+                    let href = $('#video-title').first().attr('href');
                     href = href.replace('/watch?v=','');
                     chart.videoId = href;
                     chart.save((err)=>{if(err) console.log('chart videoId update error...',err)})
