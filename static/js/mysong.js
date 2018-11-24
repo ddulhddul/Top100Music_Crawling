8e60c32b680694dadf51fb0c27d88513610eddd6
ddulh <ddulhddul@gmail.com>
2018-11-24 13:31:45 +0900

    change form to div in mysong login

--- static/js/mysong.js
+++ static/js/mysong.js
@@ -2,27 +2,27 @@ Vue.component('mysong-component', {
     template:
         `<div style="margin-top:10px;">
             <div id="temp" style="display:none;"></div>
-            <form v-if="!user" class="form-horizontal">
+            <div v-if="!user" class="form-horizontal">
                 <div class="form-group" v-bind:class="validate('userId') ? 'has-success' : 'has-error'">
                     <label for="userId" class="col-xs-2 control-label">Username</label>
                     <div class="col-xs-10">
-                        <input type="text" class="form-control" v-model="userId" placeholder="Username">
+                        <input type="text" class="form-control" v-model="userId" @keyup.enter="login" placeholder="Username">
                     </div>
                 </div>
                 <div class="form-group" v-bind:class="validate('userPw') ? 'has-success' : 'has-error'">
                     <label for="userPw" class="col-xs-2 control-label">Password</label>
                     <div class="col-xs-10">
-                        <input type="password" class="form-control" v-model="userPw" placeholder="Password">
+                        <input type="password" class="form-control" v-model="userPw" @keyup.enter="login" placeholder="Password">
                     </div>
                 </div>
                 <p class="text-danger" v-if="errorMsg">{{errorMsg}}</p>
                 <div class="form-group">
                     <div class="col-xs-offset-1 col-xs-10">
-                    <button type="submit" @click="login" class="btn btn-default">Log in</button>
+                    <button @click="login" class="btn btn-default">Log in</button>
                     <button @click="signin" class="btn btn-default">Sign in</button>
                     </div>
                 </div>
-            </form>
+            </div>
             <div v-if="user">
                 <div class="panel panel-primary">
                     <div class="panel-heading"><b>{{user.userId}}</b>'s songs</div>
